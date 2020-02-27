3b75589ffe737580b53b62ad7178d36cfa827ef1
EllentheCat <ellen_lu_0318@hotmail.com>
2020-02-27 17:50:35 +1100

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x;
+    float y;
+    printf("Enter a number: ");
+    scanf("%f", &x);//Note change of %d to%f
+    // Compete the y = line below
+    y = x*x+2x;
+    printf("y: %f\n", y);
+    return 0;
+}
